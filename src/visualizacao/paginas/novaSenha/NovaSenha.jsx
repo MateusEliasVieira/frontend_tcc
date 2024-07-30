cd48a6702c6654d68d73ad80a99a5f35606b89df
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-07-30 09:53:51 -0300

    add atualização de senha

--- src/visualizacao/paginas/novaSenha/NovaSenha.jsx
+++ src/visualizacao/paginas/novaSenha/NovaSenha.jsx
@@ -12,11 +12,11 @@ import {
   CRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import {cibFSecure, cibMinutemailer} from '@coreui/icons'
+import {cibFSecure} from '@coreui/icons'
 import Modal from "../../../components/modal/Modal";
 import "./NovaSenha.css"
-import {esconderModal} from "../../../utilidades/ManipuladorDeModal";
-import axios, {HttpStatusCode} from "axios";
+import {apresentarModal, esconderModal} from "../../../utilidades/ManipuladorDeModal";
+import axios from "axios";
 
 const NovaSenha = () => {
 
@@ -27,22 +27,49 @@ const NovaSenha = () => {
   const [novaSenha, setNovaSenha] = useState("")
   const [confirmaNovaSenha, setConfirmaNovaSenha] = useState("")
 
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
-
-  }, [])
-
+    let url = window.location.href.split("?token=")[1];
+    setUrl(url)
+  }, []);
 
   const salvarNovaSenha = () => {
-      if(novaSenha !== "" && confirmaNovaSenha !== ""){
-        if(novaSenha === confirmaNovaSenha){
-          // salvar
-        }else{
-          // senhas diferentes
-        }
-      }else{
-        // preencher as senhas
+    if (novaSenha !== "" && confirmaNovaSenha !== "") {
+      if (novaSenha === confirmaNovaSenha) {
+        // salvar
+        axios.post("http://localhost:8080/recuperacao-de-conta/nova-senha", {
+          token: url,
+          novaSenha: novaSenha
+        })
+          .then((resposta) => {
+            apresentarModal("Aviso", resposta.data.mensagem, setDisplayModal, setTituloModal, setConteudoModal)
+            setTimeout(()=>{
+              window.location.href="/#/login"
+            },5000)
+          })
+          .catch((erro) => {
+            console.log("Erro = " + erro)
+            if (erro.response) {
+              // A resposta foi recebida e o servidor respondeu com um status fora do alcance de 2xx
+              apresentarModal("Aviso", erro.response.data.titulo, setDisplayModal, setTituloModal, setConteudoModal);
+            } else if (erro.request) {
+              // A requisição foi feita, mas nenhuma resposta foi recebida
+              apresentarModal("Erro de Rede", "O servidor não respondeu. Verifique sua conexão de rede.", setDisplayModal, setTituloModal, setConteudoModal);
+            } else {
+              // Algo aconteceu na configuração da requisição que provocou um erro
+              apresentarModal("Erro", "Ocorreu um erro ao configurar a requisição.", setDisplayModal, setTituloModal, setConteudoModal);
+            }
+          })
+
+      } else {
+        // senhas diferentes
+        apresentarModal("Aviso", "As senhas são diferentes!", setDisplayModal, setTituloModal, setConteudoModal)
       }
+    } else {
+      // preencher as senhas
+      apresentarModal("Aviso", "Informe a senha!", setDisplayModal, setTituloModal, setConteudoModal)
+    }
   }
 
   return (
@@ -66,6 +93,7 @@ const NovaSenha = () => {
                         <CIcon icon={cibFSecure}/>
                       </CInputGroupText>
                       <CFormInput
+                        type="password"
                         placeholder="Senha"
                         autoComplete="senha"
                         onChange={(event) => {
@@ -78,6 +106,7 @@ const NovaSenha = () => {
                         <CIcon icon={cibFSecure}/>
                       </CInputGroupText>
                       <CFormInput
+                        type="password"
                         placeholder="Confirme a senha"
                         autoComplete="senha"
                         onChange={(event) => {
