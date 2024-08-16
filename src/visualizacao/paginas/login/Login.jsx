9186d0532007d712e9280c8432837974ae516591
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-16 15:39:01 -0300

    concertando erro de integração

--- src/visualizacao/paginas/login/Login.jsx
+++ src/visualizacao/paginas/login/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
         .then((response) => {
           if (response.status === 202) {
             localStorage.setItem('login', JSON.stringify(response.data));
-            window.location.href = "/"
+            window.location.href = "/dashboard"
           }
         })
         .catch((error) => {
