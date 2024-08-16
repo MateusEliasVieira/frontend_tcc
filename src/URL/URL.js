de09d38b962741cd5fbabf96f6d65e753e5d3836
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-16 14:20:11 -0300

    add url servidor backend em produção teste

--- src/URL/URL.js
+++ src/URL/URL.js
@@ -3,8 +3,8 @@ const ATUALIZAR_PRATICANTE = "/equoterapia/ifgoiano/urt/atualizar-praticante"
 const ATUALIZAR_USUARIO = "/equoterapia/ifgoiano/urt/atualizar-usuario/atualizar"
 const GERAR_RELATORIO_PRATICANTE = "/equoterapia/ifgoiano/urt/gerar-relatorio-de-praticante"
 const LOGIN = "/equoterapia/ifgoiano/urt/login"
-const ESQUECI_MINHA_SENHA = "/#/esqueci-minha-senha"
-const DOMINIO = "http://localhost:8080/"
+const ESQUECI_MINHA_SENHA = "/esqueci-minha-senha"
+const DOMINIO = "https://equoterapia.up.railway.app/"
 
 export {
   LOGIN,
