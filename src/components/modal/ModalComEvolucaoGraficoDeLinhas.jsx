142213314d0489438a8017e2452f2e2d373ef352
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-22 20:13:22 -0300

    concertado grafico de linhas

--- src/components/modal/ModalComEvolucaoGraficoDeLinhas.jsx
+++ src/components/modal/ModalComEvolucaoGraficoDeLinhas.jsx
@@ -1,15 +1,15 @@
-import React, {useRef, useState} from "react";
-import {CButton, CCard, CCardBody, CCardHeader, CCol, CContainer, CRow} from "@coreui/react";
-import {CChartLine} from "@coreui/react-chartjs";
-import {useReactToPrint} from "react-to-print";
-import axios, {HttpStatusCode} from "axios";
+import React, { useRef, useState } from "react";
+import { CButton, CCard, CCardBody, CCardHeader, CCol, CContainer, CRow } from "@coreui/react";
+import { CChartLine } from "@coreui/react-chartjs";
+import { useReactToPrint } from "react-to-print";
+import axios, { HttpStatusCode } from "axios";
 import {
   BUSCAR_EVOLUCAO_DO_PRATICANTE_POR_INTERVALO_DE_DATAS_POST,
 } from "../../endpoints/praticante/evolucao/Endpoint";
 import Campo from "../campos/Campo";
-import {formatarDataParaDiaMesAno} from "../../utilidades/ManipuladorDeDatas";
+import { formatarDataParaDiaMesAno } from "../../utilidades/ManipuladorDeDatas";
 import Modal from "./Modal";
-import {apresentarModal, esconderModal} from "../../utilidades/ManipuladorDeModal";
+import { apresentarModal, esconderModal } from "../../utilidades/ManipuladorDeModal";
 
 const ModalComEvolucaoGraficoDeLinhas = (props) => {
 
@@ -25,7 +25,7 @@ const ModalComEvolucaoGraficoDeLinhas = (props) => {
 
   const [dadosFrequencia, setDadosFrequencia] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   const [dadosFaltas, setDadosFaltas] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-  const [meses, setMeses] = useState(null)
+  const [meses, setMeses] = useState([]) // Iniciar com array vazio
 
   const [displayModal, setDisplayModal] = useState("none")
   const [tituloModal, setTituloModal] = useState("")
@@ -36,11 +36,39 @@ const ModalComEvolucaoGraficoDeLinhas = (props) => {
     content: () => conteudoDocumento.current,
   });
 
+  const ordenarDados = (frequencia, faltas, meses) => {
+    const months = {
+      "Jan": 0, "Fev": 1, "Mar": 2, "Abr": 3,
+      "Maio": 4, "Jun": 5, "Jul": 6, "Ago": 7,
+      "Set": 8, "Out": 9, "Nov": 10, "Dez": 11
+    };
+
+    const combined = meses.map((mes, index) => ({
+      mes,
+      frequencia: frequencia[index],
+      faltas: faltas[index]
+    }));
+
+    const sortedCombined = combined.sort((a, b) => {
+      const [monthA, yearA] = a.mes.split('/');
+      const [monthB, yearB] = b.mes.split('/');
+
+      return yearA - yearB || months[monthA] - months[monthB];
+    });
+
+    const sortedMeses = sortedCombined.map(item => item.mes);
+    const sortedFrequencia = sortedCombined.map(item => item.frequencia);
+    const sortedFaltas = sortedCombined.map(item => item.faltas);
+
+    setMeses(sortedMeses);
+    setDadosFrequencia(sortedFrequencia);
+    setDadosFaltas(sortedFaltas);
+  }
 
   const buscar = async () => {
     if (formularioDados.dataInicial !== '' && formularioDados.dataFinal !== '') {
       await axios.post(BUSCAR_EVOLUCAO_DO_PRATICANTE_POR_INTERVALO_DE_DATAS_POST,
-        JSON.stringify({...formularioDados}),
+        JSON.stringify({ ...formularioDados }),
         {
           headers: {
             'Content-Type': 'application/json',
@@ -53,10 +81,8 @@ const ModalComEvolucaoGraficoDeLinhas = (props) => {
             if (Array.isArray(response.data.frequencia) && Array.isArray(response.data.faltas) && response.data.frequencia.length === 0 && response.data.faltas.length === 0) {
               apresentarModal("Aviso", `No momento não há nenhuma informação sobre a evolução do praticante ${props.nomeCompleto} no intervalo do período ${formatarDataParaDiaMesAno(formularioDados.dataInicial)} à ${formatarDataParaDiaMesAno(formularioDados.dataFinal)}!`, setDisplayModal, setTituloModal, setConteudoModal);
             } else {
-              console.log(response.data)
-              setDadosFrequencia(response.data.frequencia);
-              setDadosFaltas(response.data.faltas);
-              setMeses(response.data.meses);
+              const { frequencia, faltas, meses } = response.data;
+              ordenarDados(frequencia, faltas, meses);
             }
           }
         })
@@ -71,7 +97,6 @@ const ModalComEvolucaoGraficoDeLinhas = (props) => {
       apresentarModal("Aviso", "Informe a data inicial e final para realizar a pesquisa de frequência do praticante!", setDisplayModal, setTituloModal, setConteudoModal);
     }
   }
-
   return (
     <div>
       <Modal
