60a68994d1441f12eb4bd19ea378250617681256
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-22 20:33:09 -0300

    concertado grafico de barras

--- src/components/modal/ModalComEvolucaoGraficoDeBarras.jsx
+++ src/components/modal/ModalComEvolucaoGraficoDeBarras.jsx
@@ -23,7 +23,7 @@ const ModalComEvolucaoGraficoDeBarras = (props) => {
 
   const [dadosFrequencia, setDadosFrequencia] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   const [dadosFaltas, setDadosFaltas] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-  const [meses, setMeses] = useState(null)
+  const [meses, setMeses] = useState([]) // Iniciar com array vazio
 
   const [displayModal, setDisplayModal] = useState("none")
   const [tituloModal, setTituloModal] = useState("")
@@ -34,9 +34,38 @@ const ModalComEvolucaoGraficoDeBarras = (props) => {
     content: () => conteudoDocumento.current,
   });
 
-  const buscar = () => {
+  const ordenarDados = (frequencia, faltas, meses) => {
+    const months = {
+      "Jan": 0, "Fev": 1, "Mar": 2, "Abr": 3,
+      "Maio": 4, "Jun": 5, "Jul": 6, "Ago": 7,
+      "Set": 8, "Out": 9, "Nov": 10, "Dez": 11
+    };
+
+    const combined = meses.map((mes, index) => ({
+      mes,
+      frequencia: frequencia[index],
+      faltas: faltas[index]
+    }));
+
+    const sortedCombined = combined.sort((a, b) => {
+      const [monthA, yearA] = a.mes.split('/');
+      const [monthB, yearB] = b.mes.split('/');
+
+      return yearA - yearB || months[monthA] - months[monthB];
+    });
+
+    const sortedMeses = sortedCombined.map(item => item.mes);
+    const sortedFrequencia = sortedCombined.map(item => item.frequencia);
+    const sortedFaltas = sortedCombined.map(item => item.faltas);
+
+    setMeses(sortedMeses);
+    setDadosFrequencia(sortedFrequencia);
+    setDadosFaltas(sortedFaltas);
+  }
+
+  const buscar = async () => {
     if (formularioDados.dataInicial !== '' && formularioDados.dataFinal !== '') {
-      axios.post(BUSCAR_EVOLUCAO_DO_PRATICANTE_POR_INTERVALO_DE_DATAS_POST,
+      await axios.post(BUSCAR_EVOLUCAO_DO_PRATICANTE_POR_INTERVALO_DE_DATAS_POST,
         JSON.stringify({...formularioDados}),
         {
           headers: {
@@ -50,9 +79,8 @@ const ModalComEvolucaoGraficoDeBarras = (props) => {
             if (Array.isArray(response.data.frequencia) && Array.isArray(response.data.faltas) && response.data.frequencia.length === 0 && response.data.faltas.length === 0) {
               apresentarModal("Aviso", `No momento não há nenhuma informação sobre a evolução do praticante ${props.nomeCompleto} no intervalo do período ${formatarDataParaDiaMesAno(formularioDados.dataInicial)} à ${formatarDataParaDiaMesAno(formularioDados.dataFinal)}!`, setDisplayModal, setTituloModal, setConteudoModal);
             } else {
-              setDadosFrequencia(response.data.frequencia);
-              setDadosFaltas(response.data.faltas);
-              setMeses(response.data.meses);
+              const {frequencia, faltas, meses} = response.data;
+              ordenarDados(frequencia, faltas, meses);
             }
           }
         })
@@ -67,7 +95,6 @@ const ModalComEvolucaoGraficoDeBarras = (props) => {
       apresentarModal("Aviso", "Informe a data inicial e final para realizar a pesquisa de frequência do praticante!", setDisplayModal, setTituloModal, setConteudoModal);
     }
   }
-
   return (
     <div>
       <Modal
