9ccf0bb54e8fd8bba0ce7ebf305138ef75a9050e
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-09-02 16:16:10 -0300

    processo de refatoração cadastro de praticante parte 6

--- src/components/tabelas/TabelaPraticante.jsx
+++ src/components/tabelas/TabelaPraticante.jsx
@@ -14,21 +14,19 @@ import {verificarSeEstaFinalizado} from "../../utilidades/VerificadorDeStatusCad
 
 const TabelaPraticante = (props) => {
 
-
   const [listaAtualizada, setListaAtualizada] = useState(props.lista);
 
   useEffect(() => {
     const atualizarLista = async () => {
       const listaComStatus = await Promise.all(props.lista.map(async (item) => {
-        const finalizado = await verificarSeEstaFinalizado(item.praticante.idPraticante);
-        return { ...item, finalizado };
+        const statusCadastro = await verificarSeEstaFinalizado(item.praticante.idPraticante);
+        return {...item, statusCadastro};
       }));
       setListaAtualizada(listaComStatus);
     };
 
     atualizarLista();
   }, [props.lista]);
-
 
   return (
     <CTable hover>
@@ -46,61 +44,64 @@ const TabelaPraticante = (props) => {
       <CTableBody>
         {
           listaAtualizada.map((item, key) => (
-              <CTableRow key={key}>
-                <CTableDataCell style={{textAlign: "center"}}>{item.praticante.idPraticante}</CTableDataCell>
-                <CTableDataCell style={{textAlign: "center"}}>
-                  {
-                    item.finalizado === true ?
-
-                        <a href={`${ATUALIZAR_PRATICANTE}?id=${item.praticante.idPraticante}`}
-                           style={{textDecoration: "none", color: "green"}}
-                           title={"Atualizar cadastro do(a) praticante " + item.nomeCompleto}>{item.nomeCompleto} <strong>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2-circle" viewBox="0 0 16 16">
-                            <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
-                            <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
-                          </svg>
-                        </strong></a>
-                      :
-                      <p>{item.nomeCompleto} <i>[<a href={`${CADASTRO_PRATICANTE}?id=${item.praticante.idPraticante}`}>Pendente</a>]</i></p>
-                  }
-                </CTableDataCell>
-                <CTableDataCell style={{textAlign: "center"}}>{item.diagnosticoClinico}</CTableDataCell>
-                <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
-                  <ModalComEvolucaoGraficoDeLinhas nomeCompleto={item.nomeCompleto}
-                                                   idPraticante={item.praticante.idPraticante}/>
-                </CTableDataCell>
-                <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
-                  <ModalComEvolucaoGraficoDeBarras nomeCompleto={item.nomeCompleto}
-                                                   idPraticante={item.praticante.idPraticante}/>
-                </CTableDataCell>
-                <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
-                  <ModalComEvolucaoGraficoDeTorta nomeCompleto={item.nomeCompleto}
-                                                  idPraticante={item.praticante.idPraticante}/>
-                </CTableDataCell>
-                <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
-                  <CButton color="" title={"Gerar relatório para " + item.nomeCompleto} style={{background: "none"}}
-                           onClick={
-                             () => {
-                               window.location.href = `${GERAR_RELATORIO_PRATICANTE}?id=${item.praticante.idPraticante}`
-                             }
-                           }>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                         className="bi bi-file-pdf-fill" viewBox="0 0 16 16">
-                      <path
-                        d="M5.523 10.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 4.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
-                      <path fill-rule="evenodd"
-                            d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m.165 11.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.6 11.6 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
-                    </svg>
-                  </CButton>
-                </CTableDataCell>
-                <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
-                  <ModalParaEvoluir nomeCompleto={item.nomeCompleto} idPraticante={item.praticante.idPraticante}/>
-                </CTableDataCell>
-              </CTableRow>
-            )
-          )}
+            <CTableRow key={key}>
+              <CTableDataCell style={{textAlign: "center"}}>{item.praticante.idPraticante}</CTableDataCell>
+              <CTableDataCell style={{textAlign: "center"}}>
+                {
+                  item.statusCadastro?.status === true ?
+                    <a href={`${ATUALIZAR_PRATICANTE}?id=${item.praticante.idPraticante}`}
+                       style={{textDecoration: "none", color: "green"}}
+                       title={"Atualizar cadastro do(a) praticante " + item.nomeCompleto}>{item.nomeCompleto} <strong>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                           className="bi bi-check2-circle" viewBox="0 0 16 16">
+                        <path
+                          d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                        <path
+                          d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                      </svg>
+                    </strong></a>
+                    :
+                    <p title={`Para o praticante ${item.nomeCompleto}, foi cadastrado ${item.statusCadastro?.finalizados} formulário(s), porém ainda resta(m) ${(Number(item.statusCadastro?.total) - Number(item.statusCadastro?.finalizados))} formulário(s) para concluir todo o cadastro.`}>{item.nomeCompleto} <i>[<a
+                      href={`${CADASTRO_PRATICANTE}?id=${item.praticante.idPraticante}`}>Pendente {item.statusCadastro?.finalizados}/{item.statusCadastro?.total}</a>]</i>
+                    </p>
+                }
+              </CTableDataCell>
+              <CTableDataCell style={{textAlign: "center"}}>{item.diagnosticoClinico}</CTableDataCell>
+              <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
+                <ModalComEvolucaoGraficoDeLinhas nomeCompleto={item.nomeCompleto}
+                                                 idPraticante={item.praticante.idPraticante}/>
+              </CTableDataCell>
+              <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
+                <ModalComEvolucaoGraficoDeBarras nomeCompleto={item.nomeCompleto}
+                                                 idPraticante={item.praticante.idPraticante}/>
+              </CTableDataCell>
+              <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
+                <ModalComEvolucaoGraficoDeTorta nomeCompleto={item.nomeCompleto}
+                                                idPraticante={item.praticante.idPraticante}/>
+              </CTableDataCell>
+              <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
+                <CButton color="" title={"Gerar relatório para " + item.nomeCompleto} style={{background: "none"}}
+                         onClick={() => {
+                           window.location.href = `${GERAR_RELATORIO_PRATICANTE}?id=${item.praticante.idPraticante}`
+                         }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                       className="bi bi-file-pdf-fill" viewBox="0 0 16 16">
+                    <path
+                      d="M5.523 10.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 4.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
+                    <path fill-rule="evenodd"
+                          d="M4 0h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2m.165 11.668c.09.18.23.343.438.419.207.075.412.04.58-.03.318-.13.635-.436.926-.786.333-.401.683-.927 1.021-1.51a11.6 11.6 0 0 1 1.997-.406c.3.383.61.713.91.95.28.22.603.403.934.417a.86.86 0 0 0 .51-.138c.155-.101.27-.247.354-.416.09-.181.145-.37.138-.563a.84.84 0 0 0-.2-.518c-.226-.27-.596-.4-.96-.465a5.8 5.8 0 0 0-1.335-.05 11 11 0 0 1-.98-1.686c.25-.66.437-1.284.52-1.794.036-.218.055-.426.048-.614a1.24 1.24 0 0 0-.127-.538.7.7 0 0 0-.477-.365c-.202-.043-.41 0-.601.077-.377.15-.576.47-.651.823-.073.34-.04.736.046 1.136.088.406.238.848.43 1.295a20 20 0 0 1-1.062 2.227 7.7 7.7 0 0 0-1.482.645c-.37.22-.699.48-.897.787-.21.326-.275.714-.08 1.103"/>
+                  </svg>
+                </CButton>
+              </CTableDataCell>
+              <CTableDataCell style={{verticalAlign: "middle", textAlign: "center"}}>
+                <ModalParaEvoluir nomeCompleto={item.nomeCompleto} idPraticante={item.praticante.idPraticante}/>
+              </CTableDataCell>
+            </CTableRow>
+          ))
+        }
       </CTableBody>
     </CTable>
-  )
+  );
 }
-export default TabelaPraticante
+
+export default TabelaPraticante;
--- src/utilidades/VerificadorDeStatusCadastro.js
+++ src/utilidades/VerificadorDeStatusCadastro.js
@@ -1,37 +1,31 @@
-import {apresentarModal} from "./ManipuladorDeModal";
-import axios, {HttpStatusCode} from "axios";
-import {STATUS_CADASTRO_GET} from "../endpoints/praticante/statusCadastro/Endpoints";
+import axios, { HttpStatusCode } from "axios";
+import { STATUS_CADASTRO_GET } from "../endpoints/praticante/statusCadastro/Endpoints";
 
 const login = JSON.parse(localStorage.getItem('login'));
 
 const verificarSeEstaFinalizado = async (idPraticante) => {
-
-  await axios.get(STATUS_CADASTRO_GET, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${login.token}`
-    },
-    params: {
-      id: idPraticante
-    }
-  })
-    .then((response) => {
-      console.log(response.data)
-      if (response.status === HttpStatusCode.Ok) {
-        if (response.data.status === true) {
-          localStorage.setItem("idPraticante", null)
-          return true
-        } else {
-          return false
-        }
+  try {
+    const response = await axios.get(STATUS_CADASTRO_GET, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${login.token}`
+      },
+      params: {
+        id: idPraticante
       }
-    })
-    .catch((error) => {
-      console.log(error.response.data)
-      return false
-    })
-}
+    });
 
+    if (response.status === HttpStatusCode.Ok) {
+      console.log(response.data)
+      return response.data;
+    } else {
+      return { finalizados: 0, status: false, total: 0 };
+    }
+  } catch (error) {
+    console.log(error.response ? error.response.data : error.message);
+    return { finalizados: 0, status: false, total: 0 };
+  }
+};
 
 export {
   verificarSeEstaFinalizado
